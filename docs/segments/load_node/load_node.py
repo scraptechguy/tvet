990ba9589a4baafe01e4c2a7e14eeff7bb1941ca
Rostislav Brož <scraptechguy@gmail.com>
2022-11-17 16:49:55 +0100

    Create `load_node.py`

---
+++ docs/segments/load_node/load_node.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+def load_node(filename, debug=False): 
+    '''Load an NODE file.'''
+    
+    node = []
+
+    f = open(filename, "r")
+
+    for line in f.readlines()[1:]:
+        if len(line) == 0:
+            continue
+        elif line[0] == '#': 
+            continue
+
+        l = line.split()
+        l = l[1:]
+
+        node.append(l)
+
+    f.close()
+
+    if debug:
+        print("number of nodes = " + str(len(node)) + ", beginning with node " + str(node[0]))
+
+def main():
+    node = load_node("docs/segments/load_node/tri_file_octdecv_1.1.node", True)
+
+if __name__ == "__main__": 
+    main()
