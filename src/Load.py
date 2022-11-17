4d04a2c58ec556e6bd6b3e7db86680aa08a30952
Rostislav Brož <scraptechguy@gmail.com>
2022-11-17 16:03:24 +0100

    Make `load_node()` work

--- src/Load.py
+++ src/Load.py
@@ -8,7 +8,25 @@ def load_ele(filename, debug=False):
 def load_node(filename, debug=False): 
     '''Load an NODE file.'''
     
+    node = []
+
     f = open(filename, "r")
+
+    for line in f.readlines()[1:]:
+        if len(line) == 0:
+            continue
+        elif line[0] == '#': 
+            continue
+
+        l = line.split()
+        l = l[1:]
+
+        node.append(l)
+
+    f.close()
+
+    if debug:
+        print("number of nodes = " + str(len(node)) + ", beginning with node " + str(node[0]))
 
 def load_face(filename, debug=False): 
     '''Load an FACE file.'''
@@ -64,9 +82,9 @@ def load_obj(filename, debug=False):
 
 
 def main():
-    # ele = load_ele("src/sample_files/tri_file_octdecv_1.ele")
-    # node = load_node("src/sample_files/tri_file_octdecv_1.1.node")
-    face = load_face("src/sample_files/tri_file_octdecv_1.1.face", True)
+    # ele = load_ele("src/sample_files/tri_file_octdecv_1.ele", True)
+    node = load_node("src/sample_files/tri_file_octdecv_1.1.node", True)
+    # face = load_face("src/sample_files/tri_file_octdecv_1.1.face", True)
     # obj_node, obj_face = load_obj("src/sample_files/tri_file_octdecv_1.obj", True)
 
 if __name__ == "__main__": 
