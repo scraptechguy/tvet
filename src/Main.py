4a4ade06343258b4bc55e29cc1f8b60ce56f7d89
Rostislav Brož <scraptechguy@gmail.com>
2024-01-11 17:19:49 +0100

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -1,118 +1,184 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import argparse
+import tkinter
+import tkinter.filedialog
+import numpy as np
 import vispy
-import Load
+import vispy.app
+import vispy.scene
+import vispy.visuals
 
-from vispy import app, scene
-from vispy.io import read_mesh, load_data_file
-from vispy.scene.visuals import Mesh
-from vispy.scene import transforms
-from vispy.visuals.filters import ShadingFilter, WireframeFilter
+def load_obj(filename, debug=False):
+    '''Load a Wavefront OBJ file.''' 
+    
+    node = []
+    face = []
+
+    f = open(filename, "r")
+
+    for line in f.readlines(): 
+        if len(line) == 0:
+            continue
+        elif line[0] == '#': 
+            continue
+
+        l = line.split()
+        
+        if l[0] == 'v':
+            node.append(list(map(float, l[1:])))
+        elif l[0] == 'f':
+            face.append(list(map(lambda x: int(x) - 1, l[1:])))
+
+    f.close()
+
+    if debug:
+        print("number of nodes = " + str(len(node)) + ", beginning with node " + str(node[0]))
+        print("number of faces = " + str(len(face)) + ", beginning with face " + str(face[0]))
+
+    return np.array(node), np.array(face)
+
+class Asteroid(object):
+    def __init__(self, filename=None, args=None):
+        self.args = args
+        self.filename = filename
+        self.vertices, self.faces = load_obj(self.filename)
+        self.size = np.max(self.vertices) - np.min(self.vertices)
+
+        self.get_properties()
+        self.plot()
+
+    def get_properties(self):
+        self.centers = []
+        self.normals = []
+
+        for face in self.faces:
+            A = self.vertices[face[0]]
+            B = self.vertices[face[1]]
+            C = self.vertices[face[2]]
+
+            T = 1/3 * (A + B + C)
+            a = B - C
+            b = C - A
+            n = np.array((np.cross(a, b))/(np.sqrt(np.dot(a, a) * np.dot(b, b))))
+
+            self.centers.append(T)
+            self.normals.append(n)
+
+        self.centers = np.array(self.centers)
+        self.normals = np.array(self.normals)
+
+    def plot(self):
+        self.canvas = vispy.scene.SceneCanvas(keys='interactive')
+        self.canvas.size = 1920, 1080
+        self.view = self.canvas.central_widget.add_view()
+
+        mesh = vispy.scene.visuals.Mesh(self.vertices, self.faces, color='gray')
+        mesh.transform = vispy.scene.transforms.MatrixTransform()
+        mesh.transform.rotate(90, (1, 0, 0))
+        mesh.transform.rotate(-45, (0, 0, 1))
+        self.view.add(mesh)
+
+        vispy.scene.visuals.Text("'e' to hide edges", anchor_x='left', pos=(20, 20), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("'f' to hide faces", anchor_x='left', pos=(20, 40), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("'s' to change scattering", anchor_x='left', pos=(20, 60), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("'q' to quit", anchor_x='left', pos=(20, 80), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        
+        vispy.scene.visuals.Text("Number of vertices: %d" %(len(self.vertices)), anchor_x='left', pos=(20, 120), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("Number of faces: %d" %(len(self.faces)), anchor_x='left', pos=(20, 140), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("Asteroid size: %f" %(self.size), anchor_x='left', pos=(20, 160), font_size=10,
+                            color='white', parent=self.canvas.scene)
+
+        wireframe_filter = vispy.visuals.filters.WireframeFilter(width=self.args.wireframe_width)
+        shading_filter = vispy.visuals.filters.ShadingFilter(shininess=self.args.shininess)
+        mesh.attach(wireframe_filter)
+        mesh.attach(shading_filter)
+
+        self.view.camera = 'turntable'
+        self.view.camera.depth_value = 1e3
+
+        def attach_headlight(view):
+            light_dir = (0, 1, 0, 0)
+            shading_filter.light_dir = light_dir[:3]
+            initial_light_dir = self.view.camera.transform.imap(light_dir)
+
+            @view.scene.transform.changed.connect
+            def on_transform_change(event):
+                transform = self.view.camera.transform
+                shading_filter.light_dir = transform.map(initial_light_dir)[:3]
+
+        attach_headlight(self.view)
+
+        shading_states = (
+            dict(shading=None),
+            dict(shading='flat'),
+            dict(shading='smooth'),
+        )
+
+        global shading_state_index
+        shading_state_index = shading_states.index(
+            dict(shading=shading_filter.shading))
+
+        wireframe_states = (
+            dict(wireframe_only=False, faces_only=False,),
+            dict(wireframe_only=True, faces_only=False,),
+            dict(wireframe_only=False, faces_only=True,),
+        )
+        
+        global wireframe_state_index
+        wireframe_state_index = wireframe_states.index(dict(
+            wireframe_only=wireframe_filter.wireframe_only,
+            faces_only=wireframe_filter.faces_only,
+        ))
+
+        def cycle_state(states, index):
+            new_index = (index + 1) % len(states)
+            return states[new_index], new_index
+
+        @self.canvas.events.key_press.connect
+        def on_key_press(event):
+            global shading_state_index
+            global wireframe_state_index
+
+            if event.key == 's':
+                state, shading_state_index = cycle_state(shading_states,
+                                                        shading_state_index)
+                for attr, value in state.items():
+                    setattr(shading_filter, attr, value)
+                mesh.update()
+            elif event.key in ['q', 'Q']:
+                vispy.app.quit()
+            elif event.key == 'e':
+                wireframe_filter.enabled = not wireframe_filter.enabled
+                mesh.update()
+            elif event.key == 'f':
+                state, wireframe_state_index = cycle_state(wireframe_states,
+                                                        wireframe_state_index)
+                for attr, value in state.items():
+                    setattr(wireframe_filter, attr, value)
+                mesh.update()
+
+        self.canvas.show()
 
 def main():
-    global view
-
     parser = argparse.ArgumentParser()
-    default_mesh = "src/sample_files/tri_file_octdecv_1.obj"
-    parser.add_argument('--mesh', default=default_mesh)
     parser.add_argument('--shininess', default=100)
     parser.add_argument('--wireframe-width', default=1)
     args, _ = parser.parse_known_args()
 
-    vertices, faces = Load.load_obj(default_mesh)
-    #vertices, faces, normals, texcoords
+    root = tkinter.Tk()
+    root.withdraw()
+    filename = tkinter.filedialog.askopenfilename()
+    asteroid = Asteroid(filename=filename, args=args)
 
-    canvas = scene.SceneCanvas(keys='interactive', bgcolor='black')
-    canvas.size = 1920, 1080
-    view = canvas.central_widget.add_view()
-
-    mesh = Mesh(vertices, faces, color='gray')
-    mesh.transform = transforms.MatrixTransform()
-    mesh.transform.rotate(90, (1, 0, 0))
-    mesh.transform.rotate(-45, (0, 0, 1))
-    view.add(mesh)
-
-    vispy.scene.visuals.Text("'e' to hide edges", anchor_x='left', anchor_y='bottom', pos=(20, 20), font_size=10,
-                        color='white', parent=canvas.scene)
-    vispy.scene.visuals.Text("'f' to hide faces", anchor_x='left', anchor_y='bottom', pos=(20, 40), font_size=10,
-                        color='white', parent=canvas.scene)
-    vispy.scene.visuals.Text("'s' to change scattering", anchor_x='left', anchor_y='bottom', pos=(20, 60), font_size=10,
-                        color='white', parent=canvas.scene)
-    vispy.scene.visuals.Text("'q' to quit", anchor_x='left', anchor_y='bottom', pos=(20, 80), font_size=10,
-                        color='white', parent=canvas.scene)
-
-    wireframe_filter = WireframeFilter(width=args.wireframe_width)
-    shading_filter = ShadingFilter(shininess=args.shininess)
-    mesh.attach(wireframe_filter)
-    mesh.attach(shading_filter)
-
-    view.camera = 'turntable'
-    view.camera.depth_value = 1e3
-
-    def attach_headlight(view):
-        light_dir = (0, 1, 0, 0)
-        shading_filter.light_dir = light_dir[:3]
-        initial_light_dir = view.camera.transform.imap(light_dir)
-
-        @view.scene.transform.changed.connect
-        def on_transform_change(event):
-            transform = view.camera.transform
-            shading_filter.light_dir = transform.map(initial_light_dir)[:3]
-
-    attach_headlight(view)
-
-    shading_states = (
-        dict(shading=None),
-        dict(shading='flat'),
-        dict(shading='smooth'),
-    )
-
-    global shading_state_index
-    shading_state_index = shading_states.index(
-        dict(shading=shading_filter.shading))
-
-    wireframe_states = (
-        dict(wireframe_only=False, faces_only=False,),
-        dict(wireframe_only=True, faces_only=False,),
-        dict(wireframe_only=False, faces_only=True,),
-    )
-    
-    global wireframe_state_index
-    wireframe_state_index = wireframe_states.index(dict(
-        wireframe_only=wireframe_filter.wireframe_only,
-        faces_only=wireframe_filter.faces_only,
-    ))
-
-    def cycle_state(states, index):
-        new_index = (index + 1) % len(states)
-        return states[new_index], new_index
-
-    @canvas.events.key_press.connect
-    def on_key_press(event):
-        global shading_state_index
-        global wireframe_state_index
-
-        if event.key == 's':
-            state, shading_state_index = cycle_state(shading_states,
-                                                    shading_state_index)
-            for attr, value in state.items():
-                setattr(shading_filter, attr, value)
-            mesh.update()
-        elif event.key in ['q', 'Q']:
-                vispy.app.quit()
-        elif event.key == 'e':
-            wireframe_filter.enabled = not wireframe_filter.enabled
-            mesh.update()
-        elif event.key == 'f':
-            state, wireframe_state_index = cycle_state(wireframe_states,
-                                                    wireframe_state_index)
-            for attr, value in state.items():
-                setattr(wireframe_filter, attr, value)
-            mesh.update()
-
-    canvas.show()
-    app.run()
+    vispy.app.run()
 
 if __name__ == "__main__":
     main()
