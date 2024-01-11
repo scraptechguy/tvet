d088e29899191145e8fe529cdbd93f6fd3c09860
Rostislav Brož <scraptechguy@gmail.com>
2024-01-11 17:20:13 +0100

    Update Test1.py

--- src/Tests/Test1.py
+++ src/Tests/Test1.py
@@ -1,35 +1,17 @@
-#!/usr/bin/env python3
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
 
-import numpy as np
-import vispy.app
-import vispy.scene
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+print(filename)
 
-def main():
+def check_filetype():
+    for index, character in enumerate(filename):
+        if character == ".":
+            if filename[index+1:] == "obj":
+                return True
+            else:
+                return False
 
-    canvas = vispy.scene.SceneCanvas()
-    view = canvas.central_widget.add_view()
-
-    pos = np.array([(1,0,0), (0,1,0), (0,0,1)])
-    markers = vispy.scene.visuals.Markers(pos=pos, face_color='red')
-    view.add(markers)
-    line1 = vispy.scene.visuals.Line(pos=np.array([(0,0,1), (0.5,0.5,0)]), color='red')
-    view.add(line1)
-    line2 = vispy.scene.visuals.Line(pos=np.array([(0,1,0), (0.5,0,0.5)]), color='red')
-    view.add(line2)
-    line3 = vispy.scene.visuals.Line(pos=np.array([(1/3,1/3,1/3), (0.66,0.66,0.66)]), color='red')
-    view.add(line3)
-
-    vertices = pos
-    faces = np.array([(0,1,2)])
-    mesh = vispy.scene.visuals.Mesh(vertices=vertices, faces=faces, color='white')
-    view.add(mesh)
-
-    view.camera = 'turntable'
-
-    axis = vispy.scene.visuals.XYZAxis(parent=view.scene)
-
-    canvas.show()
-    vispy.app.run()
-
-if __name__ == "__main__":
-    main()
+right_filetype = check_filetype()
+print(right_filetype)
