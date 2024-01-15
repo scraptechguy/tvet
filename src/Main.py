58023274e8bcb845b0f1531beae5a2c6df89dc94
Rostislav Brož <scraptechguy@gmail.com>
2024-01-15 20:46:34 +0100

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -45,8 +45,10 @@ class Asteroid(object):
         self.filename = filename
         self.vertices, self.faces = load_obj(self.filename)
         self.size = np.max(self.vertices) - np.min(self.vertices)
+        self.vertices *= 2 / self.size
 
         self.get_properties()
+        self.get_illumination(s=(0.5, 1, 0))
         self.plot()
 
     def get_properties(self):
@@ -61,7 +63,8 @@ class Asteroid(object):
             T = 1/3 * (A + B + C)
             a = B - C
             b = C - A
-            n = np.array((np.cross(a, b))/(np.sqrt(np.dot(a, a) * np.dot(b, b))))
+            n = np.cross(a, b)
+            n /= np.sqrt(np.dot(n, n))
 
             self.centers.append(T)
             self.normals.append(n)
@@ -69,16 +72,49 @@ class Asteroid(object):
         self.centers = np.array(self.centers)
         self.normals = np.array(self.normals)
 
+    def get_illumination(self, s=(1, 0, 0)):
+        self.s = s
+
+        self.mu_i = []
+        for normal in self.normals:
+            self.mu_i.append(np.dot(s, normal))
+        self.mu_i = np.array(self.mu_i)
+
+        phi_s = 1361. # W/m^2
+        self.phi_i = phi_s * self.mu_i
+        self.phi_i = np.where(self.phi_i > 0.0, self.phi_i, 0.0)
+
     def plot(self):
         self.canvas = vispy.scene.SceneCanvas(keys='interactive')
         self.canvas.size = 1920, 1080
         self.view = self.canvas.central_widget.add_view()
 
-        mesh = vispy.scene.visuals.Mesh(self.vertices, self.faces, color='gray')
+        color = np.array([1, 1, 1]) / np.max(self.phi_i)
+        face_colors = []
+        for face in self.phi_i:
+            face_colors.append(face * color)
+        # mesh = vispy.scene.visuals.Mesh(self.vertices, self.faces, color='gray')
+        mesh = vispy.scene.visuals.Mesh(self.vertices, self.faces, face_colors=face_colors)
         mesh.transform = vispy.scene.transforms.MatrixTransform()
-        mesh.transform.rotate(90, (1, 0, 0))
-        mesh.transform.rotate(-45, (0, 0, 1))
         self.view.add(mesh)
+
+        '''
+        n = np.size(self.centers, axis=0)
+        connect = np.concatenate((np.arange(0, n), np.arange(n, 2 * n)))
+        print(connect)
+        pos = np.array([self.centers, self.centers + 1 * self.normals])
+        print(pos)
+        '''
+
+        pos = np.array([self.centers, self.centers + 0.1 * self.normals])
+        connect = []
+        n = len(self.centers)
+        for i in range(n):
+            connect.append(np.array([i, n + i]))
+
+        vispy.scene.visuals.Line(pos=pos, connect=np.array(connect), color='white', parent=self.view.scene)
+        # text = vispy.scene.visuals.Text(str(i), pos=self.centers[i], font_size=10, color='white')
+        # self.view.add(text)
 
         vispy.scene.visuals.Text("'e' to hide edges", anchor_x='left', pos=(20, 20), font_size=10,
                             color='white', parent=self.canvas.scene)
@@ -86,26 +122,30 @@ class Asteroid(object):
                             color='white', parent=self.canvas.scene)
         vispy.scene.visuals.Text("'s' to change scattering", anchor_x='left', pos=(20, 60), font_size=10,
                             color='white', parent=self.canvas.scene)
-        vispy.scene.visuals.Text("'q' to quit", anchor_x='left', pos=(20, 80), font_size=10,
+        vispy.scene.visuals.Text("'n' to show normals", anchor_x='left', pos=(20, 80), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("'q' to quit", anchor_x='left', pos=(20, 100), font_size=10,
                             color='white', parent=self.canvas.scene)
         
-        vispy.scene.visuals.Text("Number of vertices: %d" %(len(self.vertices)), anchor_x='left', pos=(20, 120), font_size=10,
+        vispy.scene.visuals.Text("Number of vertices: %d" %(len(self.vertices)), anchor_x='left', pos=(20, 140), font_size=10,
                             color='white', parent=self.canvas.scene)
-        vispy.scene.visuals.Text("Number of faces: %d" %(len(self.faces)), anchor_x='left', pos=(20, 140), font_size=10,
+        vispy.scene.visuals.Text("Number of faces: %d" %(len(self.faces)), anchor_x='left', pos=(20, 160), font_size=10,
                             color='white', parent=self.canvas.scene)
-        vispy.scene.visuals.Text("Asteroid size: %f" %(self.size), anchor_x='left', pos=(20, 160), font_size=10,
+        vispy.scene.visuals.Text("Asteroid size: %f" %(self.size), anchor_x='left', pos=(20, 180), font_size=10,
                             color='white', parent=self.canvas.scene)
+        
+        vispy.scene.visuals.XYZAxis(parent=self.view.scene)
 
         wireframe_filter = vispy.visuals.filters.WireframeFilter(width=self.args.wireframe_width)
         shading_filter = vispy.visuals.filters.ShadingFilter(shininess=self.args.shininess)
         mesh.attach(wireframe_filter)
         mesh.attach(shading_filter)
 
-        self.view.camera = 'turntable'
+        self.view.camera = vispy.scene.cameras.TurntableCamera(center=(0, 0, 0))
         self.view.camera.depth_value = 1e3
-
+        '''
         def attach_headlight(view):
-            light_dir = (0, 1, 0, 0)
+            light_dir = self.s
             shading_filter.light_dir = light_dir[:3]
             initial_light_dir = self.view.camera.transform.imap(light_dir)
 
@@ -115,7 +155,7 @@ class Asteroid(object):
                 shading_filter.light_dir = transform.map(initial_light_dir)[:3]
 
         attach_headlight(self.view)
-
+        '''
         shading_states = (
             dict(shading=None),
             dict(shading='flat'),
@@ -164,6 +204,9 @@ class Asteroid(object):
                 for attr, value in state.items():
                     setattr(wireframe_filter, attr, value)
                 mesh.update()
+            elif event.key == 'n':
+                shows_normals = not shows_normals
+                mesh.update()
 
         self.canvas.show()
 
@@ -176,9 +219,27 @@ def main():
     root = tkinter.Tk()
     root.withdraw()
     filename = tkinter.filedialog.askopenfilename()
-    asteroid = Asteroid(filename=filename, args=args)
 
-    vispy.app.run()
+    def check_filetype():
+        for index, character in enumerate(filename):
+            if character == ".":
+                if filename[index+1:] == "obj":
+                    return True
+                else:
+                    return False
+
+    right_filetype = check_filetype()
+
+    if filename == '':
+        print("NO FILE WAS SELECTED")
+        vispy.app.quit()
+    elif right_filetype == False:
+        print("NEEDS TO BE AN .OBJ FILE")
+        vispy.app.quit()
+    else:
+        # filename = "src/sample_files/tri_file_octdecv_1.obj"
+        asteroid = Asteroid(filename=filename, args=args)
+        vispy.app.run()
 
 if __name__ == "__main__":
     main()
