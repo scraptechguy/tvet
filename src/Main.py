62bc703b05e1b713ecfdda0657080917e6c63954
Rostislav Brož <scraptechguy@gmail.com>
2024-01-25 10:25:08 +0100

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -10,48 +10,38 @@ import vispy.app
 import vispy.scene
 import vispy.visuals
 
-def load_obj(filename, debug=False):
-    '''Load a Wavefront OBJ file.''' 
-    
-    node = []
-    face = []
+import Load
+import Hapke
+import Shadowing
 
-    f = open(filename, "r")
+def f_lambert(f_L, mu_i, mu_e, alpha):
+    return f_L
 
-    for line in f.readlines(): 
-        if len(line) == 0:
-            continue
-        elif line[0] == '#': 
-            continue
+def f_lommel(f_L, mu_i, mu_e, alpha):
+    if mu_i + mu_e > 0.0:
+        return f_L / (mu_i + mu_e)
+    else:
+        return 0.0
 
-        l = line.split()
-        
-        if l[0] == 'v':
-            node.append(list(map(float, l[1:])))
-        elif l[0] == 'f':
-            face.append(list(map(lambda x: int(x) - 1, l[1:])))
-
-    f.close()
-
-    if debug:
-        print("number of nodes = " + str(len(node)) + ", beginning with node " + str(node[0]))
-        print("number of faces = " + str(len(face)) + ", beginning with face " + str(face[0]))
-
-    return np.array(node), np.array(face)
+def f_hapke(f_L, mu_i, mu_e, alpha):
+    return Hapke.f_hapke(f_L, mu_i, mu_e, alpha)
 
 class Asteroid(object):
-    def __init__(self, filename=None, args=None):
+    def __init__(self, args=None, filename=None):
         self.args = args
         self.filename = filename
-        self.vertices, self.faces = load_obj(self.filename)
-        self.size = np.max(self.vertices) - np.min(self.vertices)
-        self.vertices *= 2 / self.size
 
-        self.get_properties()
-        self.get_illumination(s=(0.5, 1, 0))
+        self.vertices, self.faces = Load.load_obj(self.filename)
+        self.size = np.max(self.vertices) - np.min(self.vertices)
+        self.vertices *= 1.9 / self.size
+        self.f_func = f_lambert
+
+        self.get_geometry()
+        self.get_cosines()
+        self.get_fluxes()
         self.plot()
 
-    def get_properties(self):
+    def get_geometry(self):
         self.centers = []
         self.normals = []
 
@@ -72,39 +62,73 @@ class Asteroid(object):
         self.centers = np.array(self.centers)
         self.normals = np.array(self.normals)
 
-    def get_illumination(self, s=(1, 0, 0)):
-        self.s = s
+    def get_cosines(self, s=(1, 0, 0), o=(0, 0, 1)):
+        self.s = np.array(s)
+        self.o = np.array(o)
+        self.alpha = np.arccos(np.dot(s, o))
 
-        self.mu_i = []
+        mu_i = []
+        mu_e = []
+
         for normal in self.normals:
-            self.mu_i.append(np.dot(s, normal))
-        self.mu_i = np.array(self.mu_i)
+            mu_i.append(np.dot(s, normal))
+            mu_e.append(np.dot(o, normal))
 
+        self.mu_i = np.array(mu_i)
+        self.mu_e = np.array(mu_e)
+
+        self.mu_i = np.where(self.mu_i > 0.0, self.mu_i, 0.0)
+        self.mu_e = np.where(self.mu_e > 0.0, self.mu_e, 0.0)
+
+        nu_i, nu_e = Shadowing.non(self.mu_i, self.mu_e)
+        self.nu_i = Shadowing.nu(self.faces, self.vertices, self.normals, self.centers, self.s, nu_i)
+        self.nu_e = Shadowing.nu(self.faces, self.vertices, self.normals, self.centers, self.o, nu_e)
+
+    def get_fluxes(self):
         phi_s = 1361. # W/m^2
-        self.phi_i = phi_s * self.mu_i
-        self.phi_i = np.where(self.phi_i > 0.0, self.phi_i, 0.0)
+        self.phi_i = phi_s * self.mu_i * self.nu_i
+
+        f = []
+        A_w = 0.23
+        self.f_L = A_w/(4.0*np.pi)
+        for i in range(len(self.mu_e)):
+            f.append(self.f_func(self.f_L, self.mu_i[i], self.mu_e[i], self.alpha))
+        self.f = np.array(f)
+
+        self.I = self.f * self.phi_i
+        self.phi_e = self.I * self.mu_e * self.nu_e
+
+        self.total = np.sum(self.phi_e)
+
+    def light_curve(self, n=10):
+        total = []
+
+        for i in range(n+1):
+            s = self.s
+            x, y, z = s
+            gamma = 0 + 2.0*np.pi * i/n
+            print("gamma = ", gamma/np.pi*180.0, " deg")
+
+            x_ = x * np.cos(gamma) + y * np.sin(gamma)
+            y_ = -x * np.sin(gamma) + y * np.cos(gamma)
+            z_ = z
+
+            s_ = np.array([x_, y_, z_])
+
+            self.get_cosines(s=s_)
+            self.get_fluxes()
+            total.append(self.total)
+
+        print(total)
 
     def plot(self):
         self.canvas = vispy.scene.SceneCanvas(keys='interactive')
         self.canvas.size = 1920, 1080
         self.view = self.canvas.central_widget.add_view()
 
-        color = np.array([1, 1, 1]) / np.max(self.phi_i)
-        face_colors = []
-        for face in self.phi_i:
-            face_colors.append(face * color)
-        # mesh = vispy.scene.visuals.Mesh(self.vertices, self.faces, color='gray')
-        mesh = vispy.scene.visuals.Mesh(self.vertices, self.faces, face_colors=face_colors)
+        mesh = vispy.scene.visuals.Mesh(self.vertices, self.faces, color='gray')
         mesh.transform = vispy.scene.transforms.MatrixTransform()
         self.view.add(mesh)
-
-        '''
-        n = np.size(self.centers, axis=0)
-        connect = np.concatenate((np.arange(0, n), np.arange(n, 2 * n)))
-        print(connect)
-        pos = np.array([self.centers, self.centers + 1 * self.normals])
-        print(pos)
-        '''
 
         pos = np.array([self.centers, self.centers + 0.1 * self.normals])
         connect = []
@@ -112,101 +136,162 @@ class Asteroid(object):
         for i in range(n):
             connect.append(np.array([i, n + i]))
 
-        vispy.scene.visuals.Line(pos=pos, connect=np.array(connect), color='white', parent=self.view.scene)
+        normals = vispy.scene.visuals.Line(pos=pos, connect=np.array(connect), color='white', parent=self.view.scene)
+        normals.visible = False
         # text = vispy.scene.visuals.Text(str(i), pos=self.centers[i], font_size=10, color='white')
         # self.view.add(text)
 
-        vispy.scene.visuals.Text("'e' to hide edges", anchor_x='left', pos=(20, 20), font_size=10,
+        s = vispy.scene.visuals.Line(pos=np.array([(0, 0.02, 0), self.s+ (0, 0.02, 0)]), color='yellow', parent=self.view.scene)
+        o = vispy.scene.visuals.Line(pos=np.array([(0, 0.02, 0), self.o + (0, 0.02, 0)]), color='magenta', parent=self.view.scene)
+        
+        vispy.scene.visuals.Text("'1' to show phi_i", anchor_x='left', pos=(20, 20), font_size=10,
                             color='white', parent=self.canvas.scene)
-        vispy.scene.visuals.Text("'f' to hide faces", anchor_x='left', pos=(20, 40), font_size=10,
+        vispy.scene.visuals.Text("'2' to show phi_e", anchor_x='left', pos=(20, 40), font_size=10,
                             color='white', parent=self.canvas.scene)
-        vispy.scene.visuals.Text("'s' to change scattering", anchor_x='left', pos=(20, 60), font_size=10,
+        vispy.scene.visuals.Text("'3' to show the wireframe", anchor_x='left', pos=(20, 60), font_size=10,
                             color='white', parent=self.canvas.scene)
-        vispy.scene.visuals.Text("'n' to show normals", anchor_x='left', pos=(20, 80), font_size=10,
+        vispy.scene.visuals.Text("'4' to show normals", anchor_x='left', pos=(20, 80), font_size=10,
                             color='white', parent=self.canvas.scene)
-        vispy.scene.visuals.Text("'q' to quit", anchor_x='left', pos=(20, 100), font_size=10,
+        vispy.scene.visuals.Text("'5' to show fully lit model", anchor_x='left', pos=(20, 100), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("'6' to show smooth model", anchor_x='left', pos=(20, 120), font_size=10,
                             color='white', parent=self.canvas.scene)
         
-        vispy.scene.visuals.Text("Number of vertices: %d" %(len(self.vertices)), anchor_x='left', pos=(20, 140), font_size=10,
+        vispy.scene.visuals.Text("'a' to use Lambert", anchor_x='left', pos=(20, 160), font_size=10,
                             color='white', parent=self.canvas.scene)
-        vispy.scene.visuals.Text("Number of faces: %d" %(len(self.faces)), anchor_x='left', pos=(20, 160), font_size=10,
+        vispy.scene.visuals.Text("'b' to use Lommel", anchor_x='left', pos=(20, 180), font_size=10,
                             color='white', parent=self.canvas.scene)
-        vispy.scene.visuals.Text("Asteroid size: %f" %(self.size), anchor_x='left', pos=(20, 180), font_size=10,
+        vispy.scene.visuals.Text("'c' to use Hapke", anchor_x='left', pos=(20, 200), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("'l' to create a light curve", anchor_x='left', pos=(20, 220), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("'o' to show axis", anchor_x='left', pos=(20, 240), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("'q' to quit", anchor_x='left', pos=(20, 260), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        
+        vispy.scene.visuals.Text("Number of vertices: %d" %(len(self.vertices)), anchor_x='left', pos=(20, 300), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("Number of faces: %d" %(len(self.faces)), anchor_x='left', pos=(20, 320), font_size=10,
+                            color='white', parent=self.canvas.scene)
+        vispy.scene.visuals.Text("Asteroid size: %f" %(self.size), anchor_x='left', pos=(20, 340), font_size=10,
                             color='white', parent=self.canvas.scene)
         
         vispy.scene.visuals.XYZAxis(parent=self.view.scene)
 
-        wireframe_filter = vispy.visuals.filters.WireframeFilter(width=self.args.wireframe_width)
-        shading_filter = vispy.visuals.filters.ShadingFilter(shininess=self.args.shininess)
-        mesh.attach(wireframe_filter)
+        shading_filter = vispy.visuals.filters.ShadingFilter(\
+            shading= 'smooth',
+            shininess=self.args.shininess,\
+            ambient_coefficient = 0.0,\
+            diffuse_coefficient = 1.0,\
+            specular_coefficient = 0.0,\
+            ambient_light = 'white',\
+            diffuse_light = 'white',\
+            specular_light = 'white',\
+            )
         mesh.attach(shading_filter)
+
+        wireframe_filter = vispy.visuals.filters.WireframeFilter(\
+            width=self.args.wireframe_width,\
+            color='green',\
+            wireframe_only = False,\
+            faces_only = True,\
+            enabled= False,\
+            )
+        mesh.attach(wireframe_filter)
 
         self.view.camera = vispy.scene.cameras.TurntableCamera(center=(0, 0, 0))
         self.view.camera.depth_value = 1e3
-        '''
-        def attach_headlight(view):
-            light_dir = self.s
-            shading_filter.light_dir = light_dir[:3]
-            initial_light_dir = self.view.camera.transform.imap(light_dir)
 
-            @view.scene.transform.changed.connect
-            def on_transform_change(event):
-                transform = self.view.camera.transform
-                shading_filter.light_dir = transform.map(initial_light_dir)[:3]
+        light_dir = -self.s
+        shading_filter.light_dir = light_dir[:3]
 
-        attach_headlight(self.view)
-        '''
-        shading_states = (
-            dict(shading=None),
-            dict(shading='flat'),
-            dict(shading='smooth'),
-        )
-
-        global shading_state_index
-        shading_state_index = shading_states.index(
-            dict(shading=shading_filter.shading))
-
-        wireframe_states = (
-            dict(wireframe_only=False, faces_only=False,),
-            dict(wireframe_only=True, faces_only=False,),
-            dict(wireframe_only=False, faces_only=True,),
-        )
-        
-        global wireframe_state_index
-        wireframe_state_index = wireframe_states.index(dict(
-            wireframe_only=wireframe_filter.wireframe_only,
-            faces_only=wireframe_filter.faces_only,
-        ))
-
-        def cycle_state(states, index):
-            new_index = (index + 1) % len(states)
-            return states[new_index], new_index
+        def plot_fluxes(phi):
+            shading_filter.shading = None
+            wireframe_filter.enabled = True
+            wireframe_filter.wireframe_only = False
+            wireframe_filter.faces_only = False
+            normals.visible = False
+            color = np.array([0.5, 0.5, 0.5]) / np.percentile(phi, 99)
+            face_colors = []
+            for face in phi:
+                face_colors.append(face * color)
+            mesh.set_data(self.vertices, self.faces, face_colors=face_colors)
+            mesh.update()
 
         @self.canvas.events.key_press.connect
         def on_key_press(event):
-            global shading_state_index
-            global wireframe_state_index
 
-            if event.key == 's':
-                state, shading_state_index = cycle_state(shading_states,
-                                                        shading_state_index)
-                for attr, value in state.items():
-                    setattr(shading_filter, attr, value)
-                mesh.update()
-            elif event.key in ['q', 'Q']:
+            if event.key in ['q', 'Q']:
                 vispy.app.quit()
-            elif event.key == 'e':
-                wireframe_filter.enabled = not wireframe_filter.enabled
+
+            elif event.key == '1':
+                plot_fluxes(phi=self.phi_i)
+                wireframe_filter.enabled = False
+
+            elif event.key == '2':
+                plot_fluxes(phi=self.phi_e)
+
+            elif event.key == '3':
+                shading_filter.shading = 'flat'
+                wireframe_filter.enabled = True
+                wireframe_filter.wireframe_only = True
+                wireframe_filter.faces_only = False
+                normals.visible = False
                 mesh.update()
-            elif event.key == 'f':
-                state, wireframe_state_index = cycle_state(wireframe_states,
-                                                        wireframe_state_index)
-                for attr, value in state.items():
-                    setattr(wireframe_filter, attr, value)
+
+            elif event.key == '4':
+                shading_filter.shading = 'flat'
+                wireframe_filter.enabled = True
+                wireframe_filter.wireframe_only = False
+                wireframe_filter.faces_only = False
+                normals.visible = True
+                face_colors = []
+                for i in range(len(self.faces)):
+                    face_colors.append(np.array([0.6, 0.6, 0.6]))
+                mesh.set_data(self.vertices, self.faces, face_colors=face_colors)
                 mesh.update()
-            elif event.key == 'n':
-                shows_normals = not shows_normals
+
+            elif event.key == '5':
+                shading_filter.shading = 'smooth'
+                wireframe_filter.enabled = False
+                normals.visible = False
+                face_colors = []
+                for i in range(len(self.faces)):
+                    face_colors.append(np.array([0.6, 0.6, 0.6]))
+                mesh.set_data(self.vertices, self.faces, face_colors=face_colors)
                 mesh.update()
+
+            elif event.key == '6':
+                shading_filter.shading = 'smooth'
+                wireframe_filter.enabled = False
+                normals.visible = False
+                face_colors = []
+                for i in range(len(self.faces)):
+                    face_colors.append(np.array([0.6, 0.6, 0.6]))
+                mesh.set_data(self.vertices, self.faces, face_colors=face_colors)
+                mesh.update()
+
+            elif event.key == 'a':
+                self.f_func = f_lambert
+                self.get_fluxes()
+
+            elif event.key == 'b':
+                self.f_func = f_lommel
+                self.get_fluxes()
+
+            elif event.key == 'c':
+                Hapke.B0 = 1.32
+                Hapke.minh = 0.20
+                Hapke.ming = -0.35
+                Hapke.bartheta = 10.0 * np.pi/180
+
+                Hapke.init_hapke(self.alpha)
+                self.f_func = f_hapke
+                self.get_fluxes()
+
+            elif event.key == 'l':
+                self.light_curve()
 
         self.canvas.show()
 
@@ -228,18 +313,16 @@ def main():
                 else:
                     return False
 
-    right_filetype = check_filetype()
-
     if filename == '':
         print("NO FILE WAS SELECTED")
         vispy.app.quit()
-    elif right_filetype == False:
+    elif check_filetype() == False:
         print("NEEDS TO BE AN .OBJ FILE")
         vispy.app.quit()
-    else:
-        # filename = "src/sample_files/tri_file_octdecv_1.obj"
-        asteroid = Asteroid(filename=filename, args=args)
-        vispy.app.run()
+        
+    # filename = "src/sample_files/tri_file_octdecv_1.obj"
+    asteroid = Asteroid(args=args, filename=filename)
+    vispy.app.run()
 
 if __name__ == "__main__":
     main()
