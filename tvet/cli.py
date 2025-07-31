3d32d1779d4ed6e287be095b8b51bcb611f2e559
Rostislav Brož <scraptechguy@gmail.com>
2025-08-01 00:46:49 +0200

    Add CLI options for geometry and flux outputs
    Introduces --get-geometry and --get-fluxes CLI flags to run Asteroid methods and print/save their results. Outputs centers, normals, phi_i, and phi_e to text files for easier inspection and debugging.

--- tvet/cli.py
+++ tvet/cli.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import numpy as np
 from .core import Asteroid
 
 def main():
@@ -9,14 +10,35 @@ def main():
     parser.add_argument("filename", help="Path to OBJ file")
     parser.add_argument('--shininess', default=100, help="Shininess factor for the asteroid surface")
     parser.add_argument('--wireframe-width', default=1, help="Width of the wireframe lines")
+    parser.add_argument("--get-geometry", action="store_true", help="Run get_geometry() and print results")
     parser.add_argument("--get-cosines", action="store_true", help="Run get_cosines() and print results")
+    parser.add_argument("--get-fluxes", action="store_true", help="Run get_fluxes() and print results")
     args = parser.parse_args()
 
     asteroid = Asteroid(args=args, filename=args.filename)
+    if args.get_geometry:
+        asteroid.get_geometry()
+        np.savetxt("centers.txt", asteroid.centers)
+        np.savetxt("normals.txt", asteroid.normals)
+        print("Centers:\n", asteroid.centers)
+        print("SAVED CENTERS TO centers.txt\n")
+        print("Normals:\n", asteroid.normals)
+        print("SAVED NORMALS TO normals.txt\n")
+        print("Number of centers:", len(asteroid.centers))
+        print("Number of normals:", len(asteroid.normals))
     if args.get_cosines:
         asteroid.get_cosines()
         print("mu_i:", asteroid.mu_i)
         print("mu_e:", asteroid.mu_e)
+    if args.get_fluxes:
+        asteroid.get_fluxes()
+        np.savetxt("phi_i.txt", asteroid.phi_i)
+        np.savetxt("phi_e.txt", asteroid.phi_e)
+        print("phi_i:\n", asteroid.phi_i)
+        print("SAVED phi_i TO phi_i.txt\n")
+        print("phi_e:\n", asteroid.phi_e)
+        print("SAVED phi_e TO phi_e.txt\n")
+        print("Total:", asteroid.total)
 
 if __name__ == "__main__":
     main()
